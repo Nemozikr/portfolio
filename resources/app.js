c061a76d2ba2380ceb190f60fa080e8ca756a91f
Nemozikr <ebhomrah@gmail.com>
2024-12-18 15:17:16 +0000

    initial React commit

---
+++ resources/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom';
+
+const root = createRoot(document.getElementById('main'));
+
+const App = () => {
+    return (
+        <>
+            <p>amogus</p>
+        </>
+    )
+}
+
+
+root.render(<App />)
